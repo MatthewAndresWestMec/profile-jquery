b8621fa65f19df99228901f55e91b2265b7cf95c
MatthewAndresWestMec <111930814+MatthewAndresWestMec@users.noreply.github.com>
2023-03-24 22:26:09 -0700

    finished terrible profile

--- profilePage.js
+++ profilePage.js
@@ -1,13 +1,36 @@
 $(function(){
 
+    $("ul").hide();
+    $("li").hide().css({ opacity: 0 });
+    
+    var timeoutId;
+    
+    $(".card1").on("mouseenter", function () {
+      timeoutId = setTimeout(function () {
+        $("li").fadeTo(2000, 1);
+        $("ul").slideDown(2000).show(1000);
+      }, 1000);
+    });
+    
+    $(".card1").on("mouseleave", function () {
+      clearTimeout(timeoutId);
+      $("ul").delay(500).slideUp(2000);
+    });
+    
 
-$("ul").hide()
-$("li").hide().css({ opacity: 0 })
-$(".card1").on( "mouseenter", function(){
-    $("li").fadeTo(2000,1);
-   $("ul").slideDown(2000).show(1000)});
 
-$(".card1").on( "mouseleave", function(){
-$("ul").delay(500).slideUp(2000); 
-});
+
+
+    $('.card2').click(function() {
+        $(this).find('.cover').fadeOut();
+      });
+
+
+      $('.card3').click(function() {
+        var coverWidth = $(this).find('.cover1').outerWidth();
+        $(this).find('.cover1').animate({left: -coverWidth}, function() {
+          $(this).hide();
+        });
+      });
+      
 })
